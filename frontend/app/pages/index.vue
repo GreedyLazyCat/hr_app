aa58fdaeee7c95d840476d9b09a45c55487b8624
Dmitriy Chukhlantsev <htmlweb78@gmail.com>
2026-03-04 22:35:40 +0300

    Добавил стартовый проект фронтенда на Nuxt, подключил необходимые зависимости

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <div class="text-2xl font-bold text-center w-full">
+        Проверка Tailwind и Nuxt!
+    </div>
+
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/icon"],
+  css: ["./app/assets/css/main.css"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  vite: {
+    // @ts-expect-error @nuxt/schema 4.3.1 inlines vite types, breaking compatibility with @tailwindcss/vite
+    plugins: [tailwindcss()],
+  },
+});
