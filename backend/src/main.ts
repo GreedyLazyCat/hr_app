77b0c4c2d0a7ff7751c2b95c5025d01e1b999e85
Dmitriy Chukhlantsev <htmlweb78@gmail.com>
2026-03-04 22:05:43 +0300

    Поменял порт, на котором висит бэк

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new ObjectNotFoundExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.BACKEND_PORT ?? 4000);
 }
 bootstrap();
